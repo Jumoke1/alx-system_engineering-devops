0a25d0c4df6864e4b0916b7b01fcf8cf2989642b
Jumoke1 <kazeemjumoke12@gmail.com>
2024-04-24 11:53:47 -0700

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""a python script to gather employee data from an API"""
+import requests
+import sys
+
+
+def get_employee_todo(employee_id):
+    """get employee data"""
+
+    base_url = "https://jsonplaceholder.typicode.com"
+    employee_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
+
+    employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
+    employee_name = employee_data.get("name")
+
+    todos_response = requests.get(todos_url)
+    todos = todos_response.json()
+
+    finished_tasks = [task for task in todos if task["completed"]]
+    num_finished_tasks = len(finished_tasks)
+    total_tasks = len(todos)
+
+    print(f"Employee {employee_name} is done with tasks "
+          f"({num_finished_tasks}/{total_tasks}): ")
+    for task in finished_tasks:
+        print(f"\t{task['title']}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
+    try:
+        employee_id = int(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    get_employee_todo(employee_id)
